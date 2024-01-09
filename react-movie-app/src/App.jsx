dcabc81f69cbcfe75b60bdb47d688b630ea51597
Pamela Pragides <ppragides1@my.bcit.ca>
2024-01-09 09:25:09 -0800

    added logo

--- react-movie-app/src/App.jsx
+++ react-movie-app/src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import reelPopcornLogo from '/images/reel-popcorn.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="#" target="_blank">
+          <img src={reelPopcornLogo} className="logo" alt="Reel Popcorn logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      {/* <h1>Reel Popcorn</h1> */}
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,7 +24,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
