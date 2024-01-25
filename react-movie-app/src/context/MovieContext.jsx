41dea52517257c8693999a04163426c437be6994
Charlotte Chak <cchak4@my.bcit.ca>
2024-01-25 15:49:00 -0800

    fix local storage

--- react-movie-app/src/context/MovieContext.jsx
+++ react-movie-app/src/context/MovieContext.jsx
@@ -6,46 +6,39 @@ export const MovieContext = React.createContext();
 // Create a provider component
 export const MovieProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
-  const [favourites, setFavourites] = useState([]);
+  const [favourites, setFavourites] = useState(() => {
+    const storedFavourites = sessionStorage.getItem("favourites");
+    return storedFavourites ? JSON.parse(storedFavourites) : [];
+  });
 
-  // Load favorites from local storage when the component mounts
+  // Use effect to save favorites to both local storage and session storage whenever it changes
   useEffect(() => {
-    console.log("Loading Favourites from Local Storage");
-    const storedFavourites = JSON.parse(localStorage.getItem("favourites"));
-    console.log("Stored Favourites:", storedFavourites);
-    if (storedFavourites) {
-      setFavourites(storedFavourites);
-    }
-  }, []);
-
-  useEffect(() => {
-    console.log("Saving Favourites to Local Storage");
     localStorage.setItem("favourites", JSON.stringify(favourites));
+    sessionStorage.setItem("favourites", JSON.stringify(favourites));
   }, [favourites]);
 
   useEffect(() => {
+    const fetchMovies = () => {
+      fetch(
+        "https://api.themoviedb.org/3/movie/popular?api_key=b49aeaca09961dbfa4e7d1b0fea43944"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          // Sort movies based on popularity (descending order) and then limit to top 12
+          const sortedMovies = data.results
+            .sort((a, b) => b.popularity - a.popularity)
+            .slice(0, 12);
+
+          setMovies(sortedMovies);
+        })
+        .catch((error) => {
+          console.error("Error fetching movie data:", error);
+        });
+    };
+
     // Fetch movie data when the component mounts
-    fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=b49aeaca09961dbfa4e7d1b0fea43944"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // Sort movies based on popularity (descending order) and then limit to top 12
-        const sortedMovies = data.results
-          .sort((a, b) => b.popularity - a.popularity)
-          .slice(0, 12);
-        setMovies(sortedMovies);
-        console.log(sortedMovies);
-      })
-      .catch((error) => {
-        console.error("Error fetching movie data:", error);
-      });
+    fetchMovies();
   }, []);
-
-  useEffect(() => {
-    // Save favorites to local storage whenever it changes
-    localStorage.setItem("favourites", JSON.stringify(favourites));
-  }, [favourites]);
 
   // Function to toggle a movie's favorite status
   const toggleFavourite = (movie) => {
@@ -59,14 +52,15 @@ export const MovieProvider = ({ children }) => {
   };
 
   const addToFavList = (movie) => {
-    setFavourites((prevFavourites) => [...prevFavourites, movie]);
+    const updatedFavourites = [...favourites, movie];
+    setFavourites(updatedFavourites);
   };
 
   const removeFromFavList = (movie) => {
-    setFavourites((prevFavourites) =>
-      prevFavourites.filter((fav) => fav.id !== movie.id)
-    );
+    const updatedFavourites = favourites.filter((fav) => fav.id !== movie.id);
+    setFavourites(updatedFavourites);
   };
+
   const contextValue = {
     movies,
     favourites,
