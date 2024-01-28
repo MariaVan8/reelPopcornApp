bec8aed6ba64a856b5e84c7b607b9731f1b2f392
Charlotte Chak <cchak4@my.bcit.ca>
2024-01-28 14:00:03 -0800

    fav page update

--- react-movie-app/src/pages/Favourites.jsx
+++ react-movie-app/src/pages/Favourites.jsx
@@ -1,22 +1,53 @@
-import React, { useContext } from "react";
+import React from "react";
+import { useState, useEffect, useContext } from "react";
 import { MovieContext } from "../context/MovieContext";
 
+import { Link } from "react-router-dom";
+
 function Favourites() {
-  const { favourites, removeFromFavList } = useContext(MovieContext);
+  const { favourites, removeFromFavList, movies } = useContext(MovieContext);
 
   return (
     <div>
       <h1>Favourites</h1>
-      <ul>
-        {favourites.map((movie) => (
-          <li key={movie.id}>
-            {movie.title}{" "}
-            <button onClick={() => removeFromFavList(movie)}>
-              Remove from Favorites
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className="home-container">
+        <h1>welcome to reel popcorn</h1>
+        <h2>where movie and popcorn lovers meet</h2>
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <div className="movie-container">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                <div className="movie-info">
+                  <h2>{movie.title}</h2>
+                  {/* <p>Released: {movie.release_date}</p> */}
+                  <p>
+                    Released:{" "}
+                    {new Date(movie.release_date).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "numeric",
+                    })}
+                  </p>
+                  <p>Rating: {parseFloat(movie.vote_average).toFixed(2)}</p>
+                  <p>
+                    {movie.overview.split(" ").slice(0, 20).join(" ") + "..."}
+                  </p>
+                  <Link to={`/movies/${movie.id}`}>
+                    <button>More Info</button>
+                  </Link>
+                  <button onClick={() => removeFromFavList(movie)}>
+                    Remove from Favorites
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- react-movie-app/src/pages/HomePage.jsx
+++ react-movie-app/src/pages/HomePage.jsx
@@ -1,34 +1,54 @@
-import React from 'react'
-import { useState, useEffect, useContext } from 'react';
-import { MovieContext } from '../context/MovieContext';
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+import { MovieContext } from "../context/MovieContext";
 
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function HomePage() {
   const { movies, toggleFavourite, toggleWatchlist, favourites, watchlist } =
     useContext(MovieContext);
 
   return (
-    <div className='home-container'>
+    <div className="home-container">
       <h1>welcome to reel popcorn</h1>
       <h2>where movie and popcorn lovers meet</h2>
       <ul>
-        {movies.map(movie => (
+        {movies.map((movie) => (
           <li key={movie.id}>
             <div className="movie-container">
-              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+              />
               <div className="movie-info">
                 <h2>{movie.title}</h2>
                 {/* <p>Released: {movie.release_date}</p> */}
-                <p>Released: {new Date(movie.release_date).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</p>
+                <p>
+                  Released:{" "}
+                  {new Date(movie.release_date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  })}
+                </p>
                 <p>Rating: {parseFloat(movie.vote_average).toFixed(2)}</p>
-                <p>{movie.overview.split(" ").slice(0, 20).join(" ") + "..."}</p>
+                <p>
+                  {movie.overview.split(" ").slice(0, 20).join(" ") + "..."}
+                </p>
                 <Link to={`/movies/${movie.id}`}>
                   <button>More Info</button>
                 </Link>
+                <button onClick={() => toggleFavourite(movie)}>
+                  {favourites.some((fav) => fav.id === movie.id)
+                    ? "Remove from Favorites"
+                    : "Add to Favorites"}
+                </button>
+                <button onClick={() => toggleWatchlist(movie)}>
+                  {watchlist.some((item) => item.id === movie.id)
+                    ? "Remove from Watchlist"
+                    : "Add to Watchlist"}
+                </button>
               </div>
-
             </div>
           </li>
         ))}
